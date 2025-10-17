f4af8296f35c29c722b37c045e76ed83b6f3931e
hanafiantho <hanafiantho@gmail.com>
2025-10-17 08:52:17 +0700

    split function write and read to file

--- app.go
+++ app.go
@@ -2,95 +2,30 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv"
+	"go-bank/fileops"
 )
 
 const accountBalanceFile = "balance.txt"
 
-func getBalanceFromFile() float64 {
-	data, _ := os.ReadFile(accountBalanceFile)
-	balanceText := string(data)
-	balance, _ := strconv.ParseFloat(balanceText, 64)
-
-	return balance
-}
-
-func writeBalanceToFile(balance float64) {
-	balanceText := fmt.Sprint(balance)
-	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
-}
-
 func main() {
 	var menu int
-	var accountBalance = getBalanceFromFile()
+	var accountBalance, err = fileops.GetFloatFromFile(accountBalanceFile)
+
+	if err != nil {
+		fmt.Println("Error")
+		fmt.Println(err)
+		fmt.Println("____________")
+		// panic(err) // it will show the error message and stop the application
+	}
 
 	fmt.Println("Welcome to Go Bank!")
 
 	for {
-		fmt.Println("What do you want to do?")
-		fmt.Println("1. Check balance")
-		fmt.Println("2. Deposit money")
-		fmt.Println("3. Withdraw money")
-		fmt.Println("4. Exit")
+		bankMenu()
 
 		fmt.Print("Your choice: ")
 		fmt.Scan(&menu)
 
-		// handle condition for check balance
-		// if menu == 1 {
-		// 	fmt.Println("Your balance is", accountBalance)
-		// }
-
-		// handle condition for deposit money
-		// if menu == 2 {
-		// 	var depositAmount float64
-
-		// 	fmt.Print("Input deposit amount: ")
-		// 	fmt.Scan(&depositAmount)
-
-		// 	// deposit amount validation
-		// 	if depositAmount <= 0 {
-		// 		fmt.Println("Invalid amount! deposit amount must be greater than 0")
-		// 		continue // this will makes read code start from the top of loop
-		// 	}
-
-		// 	accountBalance += depositAmount // equal to accountBalance = accountBalance + deposit
-		// 	fmt.Println("Your updated balance is", accountBalance)
-		// 	writeBalanceToFile(accountBalance)
-		// }
-
-		// handle condition for withdraw money
-		// if menu == 3 {
-		// 	var withdrawalAmount float64
-
-		// 	fmt.Print("Input withdrawal amount: ")
-		// 	fmt.Scan(&withdrawalAmount)
-
-		// 	// withdrawal amount validation (zero value)
-		// 	if withdrawalAmount <= 0 {
-		// 		fmt.Println("Invalid amount! withdrawal amount must be greater than 0")
-		// 		continue // this will makes read code start from the top of loop
-		// 	}
-
-		// 	// withdrawal amount validation (withdrawal amount greater that account balance)
-		// 	if withdrawalAmount > accountBalance {
-		// 		fmt.Println("Invalid amount! withdrawal amount must be less than account balance")
-		// 		continue // this will makes read code start from the top of loop
-		// 	}
-
-		// 	accountBalance -= withdrawalAmount // equal to accountBalance = accountBalance - withdrawalAmount
-		// 	fmt.Println("Your updated balance is", accountBalance)
-		// 	writeBalanceToFile(accountBalance)
-		// }
-
-		// handle condition for exit
-		// if menu == 4 {
-		// 	fmt.Println("Goodbye!")
-		// 	break // this will makes read code out of the loop
-		// }
-
-		// alternative of using if statement is using switch
 		switch menu {
 		case 1:
 			fmt.Println("Your balance is", accountBalance)
@@ -108,7 +43,7 @@ func main() {
 
 			accountBalance += depositAmount
 			fmt.Println("Your updated balance is", accountBalance)
-			writeBalanceToFile(accountBalance)
+			fileops.WriteFloatToFile(accountBalance, accountBalanceFile)
 		case 3:
 			var withdrawalAmount float64
 
@@ -129,13 +64,11 @@ func main() {
 
 			accountBalance -= withdrawalAmount
 			fmt.Println("Your updated balance is", accountBalance)
-			writeBalanceToFile(accountBalance)
+			fileops.WriteFloatToFile(accountBalance, accountBalanceFile)
 		default:
 			fmt.Println("Goodbye!")
 			fmt.Println("Thank you for choosing our bank!")
-			return // in switch break does not mean that it will close from the loop proccess instead of the swith case
+			return // in switch, break does not mean that it will close from the loop proccess instead of the swith case
 		}
 	}
-
-	// fmt.Println("Thank you for choosing our bank!")
 }
